88d95898ab1462b3c9c7414b9a5f27db493758c1
xxxxyn8 <maybelee08@gmail.com>
2025-06-05 15:47:49 +0900

    Update 00_emoji.py

--- pages/00_emoji.py
+++ pages/00_emoji.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st. image(https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeW80aXR4a3YyNnUxYnQ1N3JpaXV6aDVuamhjM2x5ZnhkdjZvbHZ0eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/yFQ0ywscgobJK/giphy.gif) 
+st. image("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeW80aXR4a3YyNnUxYnQ1N3JpaXV6aDVuamhjM2x5ZnhkdjZvbHZ0eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/yFQ0ywscgobJK/giphy.gif") 
